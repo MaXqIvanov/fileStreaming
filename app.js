781f417356f47ccaa53176e56fa4d83d0260bf44
max.i <max.i@konomic.com>
2023-12-29 14:15:48 +0300

    update

--- app.js
+++ app.js
@@ -1,38 +1,10 @@
 const express = require('express');
-const fs = require('fs');
 const cors = require('cors');
-const path = require('path');
+const router = require('./video/Video');
 const app = express();
 app.use(cors());
 
-app.get('/video', (req, res) => {
-	const videoPath = `assets/video-user-3.mp4`;
-	const videoStat = fs.statSync(videoPath);
-	const fileSize = videoStat.size;
-	const videoRange = req.headers.range;
-	if (videoRange) {
-		const parts = videoRange.replace(/bytes=/, '').split('-');
-		const start = parseInt(parts[0], 10);
-		const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-		const chunksize = end - start + 1;
-		const file = fs.createReadStream(videoPath, { start, end });
-		const head = {
-			'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-			'Accept-Ranges': 'bytes',
-			'Content-Length': chunksize,
-			'Content-Type': 'video/mp4'
-		};
-		res.writeHead(206, head);
-		file.pipe(res);
-	} else {
-		const head = {
-			'Content-Length': fileSize,
-			'Content-Type': 'video/mp4'
-		};
-		res.writeHead(200, head);
-		fs.createReadStream(videoPath).pipe(res);
-	}
-});
+app.use(router);
 
 app.listen(5010, () => {
 	console.log('Listening on port 5010');
